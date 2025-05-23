d179784810715fc610c0a1d2a3c6688daab9e97d
marthamareal <marthamareal@gmail.com>
2025-05-23 15:33:23 +0300

    - Add User registration route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const userRoutes = require('./routes/userRoutes')
 
 // MiddleWares
 app.use(express.json())
+
+// Routes
+app.use('/api/auth', userRoutes)
 
 module.exports = app
