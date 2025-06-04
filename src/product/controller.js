c73d7ec3fdb4e4614f98e24c884ec167a191d68d
marthamareal <marthamareal@gmail.com>
2025-06-04 11:45:19 +0300

    - add getProduct route

--- src/product/controller.js
+++ src/product/controller.js
@@ -3,34 +3,49 @@ const sanitize = require("../utils");
 const { productInPutSchema, productOutPutSchema } = require("./schema");
 
 exports.createProduct = async (req, res) => {
-  const validated = productInPutSchema.safeParse(req.body);
-  if (!validated.success) {
-    return res
-      .status(400)
-      .json({ error: validated.error.flatten().fieldErrors });
-  }
-  const data = validated.data;
+    const validated = productInPutSchema.safeParse(req.body);
+    if (!validated.success) {
+        return res
+            .status(400)
+            .json({ error: validated.error.flatten().fieldErrors });
+    }
+    const data = validated.data;
 
-  try {
+    try {
 
-    // Create product in the database
-    const product = await prisma.product.create({ data });
-    const safeProduct = productOutPutSchema.parse(product)
-    return res.status(201).json(safeProduct);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ message: "Server error" });
-  }
+        // Create product in the database
+        const product = await prisma.product.create({ data });
+        const safeProduct = productOutPutSchema.parse(product)
+        return res.status(201).json(safeProduct);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Server error" });
+    }
 };
 
 exports.getProducts = async (req, res) => {
-  try {
-    // Get products from the database
-    const products = await prisma.product.findMany();
-    const safeProducts = sanitize(products);
-    return res.status(201).json(safeProducts);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ message: "Server error" });
-  }
+    try {
+        // Get products from the database
+        const products = await prisma.product.findMany();
+        const safeProducts = sanitize(products);
+        return res.status(201).json(safeProducts);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+exports.getProduct = async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        // Get product from the database
+        const product = await prisma.product.findUnique({ where: { id } });
+        if (!product) return res.status(404).json({ message: "Product with given Id is not found" })
+
+        const safeProduct = productOutPutSchema.parse(product);
+        return res.status(201).json(safeProduct);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Server error" });
+    }
 };
--- src/product/routes.js
+++ src/product/routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createProduct, getProducts } = require("./controller");
+const { createProduct, getProducts, getProduct } = require("./controller");
 const { protect, isAdminUser} = require("../auth/middleware");
 
 // Register user
 router.post("/", protect, isAdminUser, createProduct);
 router.get("/", protect, getProducts);
+router.get("/:id", protect, getProduct);
 
 module.exports = router;
