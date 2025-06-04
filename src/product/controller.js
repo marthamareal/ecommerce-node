b2891d2fc348f7a152808d7cf1704c2734cf7132
marthamareal <marthamareal@gmail.com>
2025-06-04 12:11:08 +0300

    - add update product

--- src/product/controller.js
+++ src/product/controller.js
@@ -60,8 +60,34 @@ exports.deleteProduct = async (req, res) => {
                 .status(404)
                 .json({ message: "Product with given Id is not found" });
 
-        const deleted = await prisma.product.delete({ where: { id }})
-        if(deleted) return res.status(200).json({message: "Product is successfully deleted"});
+        const deleted = await prisma.product.delete({ where: { id } })
+        if (deleted) return res.status(200).json({ message: "Product is successfully deleted" });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+exports.updateProduct = async (req, res) => {
+    const validated = productInPutSchema.partial().safeParse(req.body);
+    if (!validated.success) {
+        return res
+            .status(400)
+            .json({ error: validated.error.flatten().fieldErrors });
+    }
+    const data = validated.data;
+    const id = parseInt(req.params.id);
+
+    try {
+        const product = await prisma.product.findUnique({ where: { id } });
+        if (!product)
+            return res
+                .status(404)
+                .json({ message: "Product with given Id is not found" });
+        // update product in the database
+        const updatedproduct = await prisma.product.update({ where: { id }, data });
+        const safeProduct = productOutPutSchema.parse(updatedproduct);
+        return res.status(201).json(safeProduct);
     } catch (err) {
         console.error(err);
         return res.status(500).json({ message: "Server error" });
--- src/product/routes.js
+++ src/product/routes.js
@@ -5,13 +5,15 @@ const {
   getProducts,
   getProduct,
   deleteProduct,
+  updateProduct
 } = require("./controller");
-const { protect, isAdminUser} = require("../auth/middleware");
+const { protect, isAdminUser } = require("../auth/middleware");
 
 // Register user
 router.post("/", protect, isAdminUser, createProduct);
 router.get("/", protect, getProducts);
 router.get("/:id", protect, getProduct);
 router.delete("/:id", protect, deleteProduct);
+router.patch("/:id", protect, isAdminUser, updateProduct);
 
 module.exports = router;
